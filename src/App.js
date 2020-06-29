abca70cca40015d08c1be6ebf3468df9c78247a2
_E.A||E.J_ <48546116+amiokuefe1@users.noreply.github.com>
2020-06-29 04:30:13 +0100

    Update App.js
    I changed the API url prefix from http to https

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ get_weatherIcon(icon, rangeId){
     // the above retrieves the user data input from the form
 
     if(city&&country){
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     // the above fetch link will fetch london,uk by default if there's no api request
 
       const response = await api_call.json();
